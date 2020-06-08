0687e157e75ab73c8be1a472dc0a9b208342ec2b
imane <benbenimane@hotmail.com>
2020-06-08 22:24:19 +0200

    some css issues

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import {ReactComponent as BellIcon}  from './icons/bell.svg';
+import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
+import { ReactComponent as CaretIcon } from './icons/caret.svg';
+import { ReactComponent as PlusIcon } from './icons/plus.svg';
+import { ReactComponent as CogIcon } from './icons/cog.svg';
+import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
+import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
+import { ReactComponent as BoltIcon } from './icons/bolt.svg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar>
+      {/* <NavItem icon  =  {'\uD83D\uDE42'}/>
+      <NavItem icon  =  {'\uD83D\uDE42'}/>
+      <NavItem icon  =  {'\uD83D\uDE42'}/> */}
+      <NavItem icon={<PlusIcon />} />
+      <NavItem icon={<BellIcon />} />
+      <NavItem icon={<MessengerIcon />} />
+      <NavItem icon={<CaretIcon />} >
+        <DropDownMenu/>
+        </NavItem>
+
+    </Navbar>
+  );
+}
+
+function DropDownMenu(){
+
+  function DropDownItem(props){
+    return(
+      <a href="#" className="menu-item">
+        <span className="icon-button">{props.leftIcon}
+        </span>
+        {props.children}
+        <span className=" icon-right">
+        {props.rightIcon}
+        </span>
+      </a>
+    )
+    
+  }
+  return(
+    <div className='dropdown'>
+      <DropDownItem>my profile</DropDownItem>
+      <DropDownItem 
+      leftIcon={<CogIcon/>}
+      rightIcon={<ChevronIcon/>}>
+        my profile
+      </DropDownItem>
     </div>
+  );
+}
+function  Navbar({children}){
+  return(
+    <nav className="navbar">
+    <ul className="navbar-nav">
+    {children}
+    </ul>
+  </nav>
+  );
+}
+
+function  NavItem({icon,children}){
+  const [open, setOpen] = useState(false);
+  return(
+    <li className="nav-item"> 
+      <a href="#" className="icon-button" onClick={()=> setOpen(!open)}>
+        {icon}
+      </a>
+      {open && children}
+    </li>
   );
 }
 
